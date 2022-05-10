230aacc3dce2079ceb832388a4d6e510648b88c3
Samm <gmc103709@gmail.com>
2022-05-10 13:44:43 -0300

    improved layout

--- js/index.js
+++ js/index.js
@@ -1,176 +1,181 @@
-const val = document.getElementById('val')
-const form = document.querySelector('form')
-const listMsg = document.querySelector('.list-msg')
-let lis = listMsg.getElementsByTagName('li')
+const val = document.getElementById("val");
+const form = document.querySelector("form");
+const listMsg = document.querySelector(".list-msg");
+let lis = listMsg.getElementsByTagName("li");
 
-const profileInfoName = document.querySelector('.profile-info p')
-profileInfoName.textContent = 'Johnny'
+const profileInfoName = document.querySelector(".profile p");
+profileInfoName.textContent = "Johnny";
 
-
-let friendsM = getItem()
-
+async function setItem() {
+  await localStorage.setItem("dides", JSON.stringify(friendsChat));
+}
 
 function getItem() {
-    let getMessages = localStorage.getItem('dides')
-    getMessages = JSON.parse(getMessages)
+  let getMessages = localStorage.getItem("dides");
+  getMessages = JSON.parse(getMessages);
 
-    return getMessages ? getMessages : [
+  return getMessages
+    ? getMessages
+    : [
         {
-            name: 'Joh',
-            message: 'Hello everyone!',
-            age: 24
+          name: "Joh",
+          message: "Hello everyone!",
+          age: 24,
         },
         {
-            name: 'Mary',
-            message: 'Hi friends, wassup',
-            age: 45
+          name: "Mary",
+          message: "Hi friends, wassup",
+          age: 45,
         },
         {
-            name: 'Johnny',
-            message: 'Hey everything is fine',
-            age: 34
+          name: "Johnny",
+          message: "Hey everything is fine",
+          age: 34,
         },
         {
-            name: 'Silv',
-            message: 'whats the plan for today?',
-            age: 55
+          name: "Silv",
+          message: "whats the plan for today?",
+          age: 55,
         },
-    ]
+      ];
 }
-
-
-
-function setItem() {
-    localStorage.setItem('dides', JSON.stringify(friendsM))
-}
+let friendsChat = getItem();
 
 function genereteM(obj) {
-    const li = document.createElement('li')
-    const btnProfile = document.createElement('button')
-    btnProfile.classList.add('btnProfile')
-
-    li.appendChild(btnProfile)
-
-    const pName = document.createElement('p')
-    pName.classList.add('pName')
-    pName.textContent = obj.name
-
-    const p = document.createElement('p')
-    p.classList.add('messages')
-    p.textContent = obj.message
-
-    const div = document.createElement('div')
-    div.classList.add('pMessages')
-
-    div.appendChild(pName)
-    div.appendChild(p)
+  const li = document.createElement("li");
+  const btnProfile = document.createElement("button");
+  btnProfile.classList.add("btnProfile");
 
 
-    const btn = document.createElement('button')
-    btn.classList.add('btnExclude')
+  const pName = document.createElement("p");
+  pName.classList.add("pName");
+  pName.textContent = obj.name;
 
-    const divDelete = document.createElement('div')
-    divDelete.appendChild(btn)
-    divDelete.classList.add('box-delete')
-    const iDelete = document.createElement('i')
-    iDelete.className = 'fas fa-trash'
+  const p = document.createElement("p");
+  p.classList.add("messages");
+  p.textContent = obj.message;
 
-    const iEdit = document.createElement('i')
-    iEdit.className = 'fas fa-edit'
+  const div = document.createElement("div");
+  div.classList.add("messages-box");
 
-    btn.appendChild(iEdit)
-    btn.appendChild(iDelete)
-    div.appendChild(divDelete)
+  const box = document.createElement('div')
 
-    li.appendChild(div)
+  div.appendChild(btnProfile);
+
+  box.appendChild(pName);
+  box.appendChild(p);
+  div.appendChild(box)
 
 
-    return li
+  const btn = document.createElement("button");
+  btn.classList.add("btn-exclude");
+
+  const btnEdit = document.createElement("button");
+  btnEdit.classList.add("btn-edit");
+
+  const divDelete = document.createElement("div");
+
+  divDelete.classList.add("box-delete");
+  const iDelete = document.createElement("i");
+  iDelete.className = "fas fa-trash";
+
+  const iEdit = document.createElement("i");
+  iEdit.className = "fas fa-edit";
+
+  btn.appendChild(iDelete);
+  btnEdit.appendChild(iEdit);
+
+  divDelete.appendChild(btn);
+  divDelete.appendChild(btnEdit);
+
+  li.appendChild(div);
+  li.appendChild(divDelete);
+
+
+  return li;
 }
-
 
 function render() {
-    listMsg.innerHTML = ''
-    friendsM.forEach(friends => {
-        listMsg.appendChild(genereteM(friends))
-    })
-    getItem()
+  listMsg.innerHTML = "";
+  friendsChat.forEach((message) => {
+    listMsg.appendChild(genereteM(message));
+  });
+  getItem();
 }
 
-
-function send(typing){
-    friendsM.push({
-            name: 'Johnny',
-            message: typing,
-            age: 34
-        })
-   setItem()
+function send(typing) {
+  friendsChat.push({
+    name: "Johnny",
+    message: typing,
+    age: 34,
+  });
+  setItem();
 }
-
 
 function ulEvent(e) {
-    let current = e.target
-    let li = current
+  let current = e.target;
+  let li = current;
 
-    while(li.nodeName !== 'LI'){
-        li = li.parentElement
-    }
 
-    let currentIndex = [...lis].indexOf(li)
-    
-    if(current.className === 'fas fa-trash') {
-        friendsM.splice(currentIndex, 1)
-        render()
-        setItem()
-    } else if (current.className === 'fas fa-edit') {
-        const editBox = document.createElement('div')
-        editBox.classList.add('edit')
+  while (li.nodeName !== "LI") {
+    li = li.parentElement;
+  }
 
-        const editInput = document.createElement('input')
-        editInput.classList.add('editMessage')
+  let currentIndex = [...lis].indexOf(li);
 
-        editBox.appendChild(editInput)
+  if (current.className === "fas fa-trash") {
+    friendsChat.splice(currentIndex, 1);
+    render();
+    setItem();
+  } else if (current.className === "fas fa-edit") {
+    const editBox = document.createElement("div");
+    editBox.classList.add("edit");
 
-        const editButton = document.createElement('button')
-        editButton.classList.add("edit-btn")
-        editButton.textContent = 'Edit'
-        editButton.addEventListener('click', function(){
-           const pM = li.querySelector('.editMessage').value
-           friendsM[currentIndex].message = pM
-           render()
-           setItem()
-        })
+    const editInput = document.createElement("input");
+    editInput.classList.add("editMessage");
 
-        editBox.appendChild(editButton)
+    editBox.appendChild(editInput);
 
-        const cancel = document.createElement('button')
-        cancel.classList.add('cancel-btn')
-        cancel.textContent = 'Cancel'
+    const editButton = document.createElement("button");
+    editButton.classList.add("edit-btn");
+    editButton.textContent = "Edit";
+    editButton.addEventListener("click", function () {
+      const pM = li.querySelector(".editMessage").value;
+      friendsChat[currentIndex].message = pM;
+      render();
+      setItem();
+    });
 
-        editBox.appendChild(cancel)
-        cancel.addEventListener('click', function(){ editBox.remove()})
-        const div = li.querySelector('.pMessages')
+    editBox.appendChild(editButton);
 
-        div.appendChild(editBox)
-    }
+    const cancel = document.createElement("button");
+    cancel.classList.add("cancel-btn");
+    cancel.textContent = "Cancel";
+
+    editBox.appendChild(cancel);
+    cancel.addEventListener("click", function () {
+      editBox.remove();
+    });
+    const div = li.querySelector(".messages-box");
+
+    div.appendChild(editBox);
+  }
 }
 
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
 
-form.addEventListener('submit', function(e){
-    e.preventDefault()
+  if (!val.value) {
+    return;
+  } else {
+    send(val.value);
+  }
+  render();
 
-    if(!val.value){
-        return
-    } else {
-        send(val.value)
-    }
-    render()
+  val.value = "";
+  val.focus();
+});
 
-    val.value = ''
-    val.focus()
-})
+listMsg.addEventListener("click", ulEvent);
 
-
-listMsg.addEventListener('click', ulEvent)
-
-render()
+render();
